c57fbd7d380c58acb26dc1d6c88558ed7ba06760
Takeshi Kada <t.kada.1985@gmail.com>
2019-02-14 22:38:02 +0900

    Initial Commit

---
+++ Assets/EquirectRenderer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class EquirectRenderer : MonoBehaviour
+{
+    RenderTexture m_cubemap;
+
+	[SerializeField]
+    RenderTexture m_EquirectTexture; //このテクスチャをCanvasなどを使って最前面に描画
+
+    Camera m_Camera;
+
+     void Start()
+    {
+        m_Camera = this.GetComponent<Camera>();
+
+        m_cubemap = new RenderTexture(1024, 1024, 24);
+        m_cubemap.dimension = UnityEngine.Rendering.TextureDimension.Cube;  //Cubemap形式に設定
+    }
+
+    private void LateUpdate()
+    {
+        m_Camera.RenderToCubemap(m_cubemap);    //カメラの映像をCubemapに撮像
+
+        m_cubemap.ConvertToEquirect(m_EquirectTexture); //CubemapをEquirectangularに変換
+    }
+}
+
